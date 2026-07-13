4c59312097ebe5ab4546ffdb0282ce11cba08119
DGPC-4\Gandeev <digital@ubon.in>
2026-07-13 17:06:39 +0530

    5000 to 3000 port

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const API = axios.create({
-    baseURL: "http://200.97.166.234:5000/api"
+    baseURL: "http://200.97.166.234:3000/api"
 });
 
 API.interceptors.request.use(
